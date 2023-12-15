2f5f9f51c462b157555565a58f80ac33eaf9036d
MikeIsAStar <99037623+MikeIsAStar@users.noreply.github.com>
2023-12-15 04:00:30 -0500

    [MKW] Prevent the game from crashing if a cannon is entered on a course without one

--- payload/import/mkwSystem.hpp
+++ payload/import/mkwSystem.hpp
@@ -7,6 +7,36 @@ namespace mkw::system
 
 #if RMC
 
+// https://github.com/riidefi/mkw/blob/master/source/game/system/CourseMap.hpp#L359-L373
+class MapdataCannonPoint
+{
+public:
+    enum class CannonType : s16 {
+        Direct,
+        Curved,
+        CurvedSlow,
+
+        Default = -1,
+    };
+
+    struct Data {
+        egg::Vector3f position;
+        egg::Vector3f rotation;
+        u16 id;
+        CannonType cannonType;
+    };
+
+    MapdataCannonPoint(const Data* data)
+      : m_data(data)
+    {
+    }
+
+private:
+    const Data* m_data;
+};
+
+static_assert(sizeof(MapdataCannonPoint) == 0x4);
+
 // https://github.com/riidefi/mkw/blob/master/source/game/system/CourseMap.hpp#L543-L557
 class MapdataItemPoint
 {
