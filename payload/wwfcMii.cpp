b52b1c43293a80332c2222650d2374e53fe7557b
Palapeli <26661008+mkwcat@users.noreply.github.com>
2026-03-26 04:00:20 -0400

    Payload: Clear hidden Mii info for SAKE

--- payload/wwfcMii.cpp
+++ payload/wwfcMii.cpp
@@ -8,10 +8,12 @@
 namespace wwfc::Mii
 {
 
-void ClearUserMiiInfo(mkw::Net::UserHandler::Packet* packet);
+static void ClearUserMiiInfo(mkw::Net::UserHandler::Packet* packet);
+static int
+RFLGetStoreDataOverride(RFLiStoreData* miiData, int source, u16 index);
 
 WWFC_DEFINE_PATCH = Patch::CallWithCTR(
-    WWFC_PATCH_LEVEL_FEATURE, //
+    WWFC_PATCH_LEVEL_BUGFIX, //
     RMCXD_PORT(0x80663178, 0x80661094, 0x806627E4, 0x80651490, DEMOTODO), //
     ASM_LAMBDA(
         ( : ASM_IMPORT(i, ClearUserMiiInfo)),
@@ -30,19 +32,23 @@ WWFC_DEFINE_PATCH = Patch::CallWithCTR(
     )
 );
 
+WWFC_DEFINE_PATCH = Patch::CallWithCTR(
+    WWFC_PATCH_LEVEL_BUGFIX, //
+    RMCXD_PORT(0x80672E80, 0x8066B71C, 0x806724EC, 0x806611D8, DEMOTODO), //
+    ASM_LAMBDA(
+        ( : ASM_IMPORT(i, RFLGetStoreDataOverride)),
+        // clang-format off
+        lhz     r5, 0x8(r1);
+        mr      r3, r31;
+        li      r4, 0;
+        b       %[RFLGetStoreDataOverride];
+        // clang-format on
+    ),
+    5
+);
+
 static void ClearMiiInfo(RFLiStoreData* miiData)
 {
-    // Check if this is a guest "no name" Mii
-    [[gnu::longcall]] bool RFLSearchOfficialData( //
-        const RFLCreateID* id, u16* index
-    ) AT(RMCXD_PORT(0x800CA820, 0x800CA780, 0x800CA740, 0x800CA880, DEMOTODO));
-
-    u16 discard;
-    if (RFLSearchOfficialData(&miiData->data.createID, &discard)) {
-        // It's a guest Mii
-        return;
-    }
-
     // Mainly just clear the create ID stuff, which you can get the MAC
     // address from
     miiData->data.createID.miiID = 0x80000000;
@@ -80,19 +86,43 @@ static void ClearMiiInfo(RFLiStoreData* miiData)
     miiData->checksum = RFLiCalculateCRC(miiData, sizeof(RFLiStoreData));
 }
 
-void ClearUserMiiInfo(mkw::Net::UserHandler::Packet* packet)
+static void ClearUserMiiInfo(mkw::Net::UserHandler::Packet* packet)
 {
     // Clear hidden Mii info
     // Mii Group count _should_ be 2, but could be higher if the code has been
     // messed with
     for (u32 i = 0; i < packet->miiGroupCount; i++) {
-        ClearMiiInfo(&packet->miiData[i]);
+        // Check if this is a guest "no name" Mii
+        [[gnu::longcall]] bool RFLSearchOfficialData( //
+            const RFLCreateID* id, u16* index
+        ) AT(RMCXD_PORT(0x800CA820, 0x800CA780, 0x800CA740, 0x800CA880, DEMOTODO));
+
+        u16 discard;
+        if (!RFLSearchOfficialData(
+                &packet->miiData[i].data.createID, &discard
+            )) {
+            // It's not a guest Mii
+            ClearMiiInfo(&packet->miiData[i]);
+        }
     }
 
     // Send Mii data to the server
     wwfc::GPReport::ReportB64Encode(
         "wl:mkw_user", packet, sizeof(mkw::Net::UserHandler::Packet)
     );
+}
+
+static int
+RFLGetStoreDataOverride(RFLiStoreData* miiData, int source, u16 index)
+{
+    [[gnu::longcall]] int RFLGetStoreData( //
+        RFLiStoreData* data, int source, u16 index
+    ) AT(RMCXD_PORT(0x800C7DF0, 0x800C7D50, 0x800C7D10, 0x800C7E50, DEMOTODO));
+
+    *miiData = {};
+    int result = RFLGetStoreData(miiData, source, index);
+    ClearMiiInfo(miiData);
+    return result;
 }
 
 } // namespace wwfc::Mii
